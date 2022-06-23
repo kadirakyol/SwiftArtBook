5fbdfcf0d7394bdea403737c98ca4e6ef4686abe
kadirakyol <kadir.akyol.ce@gmail.com>
2022-06-23 12:18:25 +0300

    Data Saved

--- ArtBook/DetailsVC.swift
+++ ArtBook/DetailsVC.swift
@@ -6,24 +6,75 @@
 //
 
 import UIKit
+import CoreData
 
-class DetailsVC: UIViewController {
+class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var nameText: UITextField!
-    
     @IBOutlet weak var artistText: UITextField!
-    
     @IBOutlet weak var yearText: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        // Recognizers
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let imageTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageTapRecognizer)
 
     }
 
     @IBAction func saveButton(_ sender: Any) {
+        let appDelagate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelagate.persistentContainer.viewContext
+        
+        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        
+        // Attributes
+        
+        newPainting.setValue(nameText.text!, forKey: "name")
+        newPainting.setValue(artistText.text, forKey: "artist")
+        if let year = Int(yearText.text!){
+            newPainting.setValue(year, forKey: "year")
+        }
+        newPainting.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image?.jpegData(compressionQuality: 0.5)
+        
+        newPainting.setValue(data, forKey: "image")
+        do {
+            try context.save()
+            print("Success!")
+        } catch {
+            print("Error!")
+        }
+       
+        
+        
+            }
+    
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @objc func selectImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.editedImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
     
     

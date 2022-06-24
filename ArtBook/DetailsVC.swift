bf9e46422e1ac942f8fcb492fc91a739bdedc436
kadirakyol <kadir.akyol.ce@gmail.com>
2022-06-24 10:37:47 +0300

    do try catch

--- ArtBook/DetailsVC.swift
+++ ArtBook/DetailsVC.swift
@@ -54,6 +54,10 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
         } catch {
             print("Error!")
         }
+        
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "newData"), object: nil)
+        self.navigationController?.popViewController(animated: true)
+        
        
         
         
